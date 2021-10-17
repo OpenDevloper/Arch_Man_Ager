17a162578387b31956820ab2c03c97b2c48baa25
OpenDevloper <89633504+OpenDevloper@users.noreply.github.com>
2021-10-17 10:49:01 +0530

    Add files via upload

--- arch_man_ager.sh
+++ arch_man_ager.sh
@@ -5,8 +5,9 @@ welcome() { \
     dialog --colors --title "\Z7\ZbUpdates COnfig" --yes-label "Sure!" --no-label "NO. exit please." --yesno "\Z4we are going to install a few packages and edit the /etc/pacman.conf to make things work better! do you want to continue!!!!!?" 8 60
 }
 
-
-
+error() { \
+	clear; printf "ERROR:\\n%s\\n" "$1" >&2; exit 1;
+}
 
 
 
@@ -14,18 +15,21 @@ welcome() { \
 
 welcome || error "User choose to exit."
 
-configureaura() { \
-    echo "#########################################################"
-    echo "## installing aura package manager ##################### ##"
+
+configAura() { \
+    echo "## Installaing The YAY aur helper ##################### ##"
     echo "#########################################################"
     sudo pacman -S git
-    git clone https://aur.archlinux.org/aura-bin.git
-    cd aura-bin
+    sudo pacman -S go
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
     makepkg
-    sudo pacman -U aura-bin-3.2.6-1-x86_64.pkg.tar.zst
+    sudo pacman -U yay-11.0.2-1-x86_64.pkg.tar.zst
     
     
 }
+
+
 
 updatesystem() { \
 	echo "#########################################"
@@ -35,13 +39,18 @@ updatesystem() { \
 	echo "Update completed. moving on!"
 }
 
+updatesystem || error "Error using Pacman -Syyu to update the system"
 windowmanagerconfig() { \
 	dialog --colors --title "\Z7\zbXorg and Window Manager!" --yes-label "Sure!" --no-label "No, exit." --yesno "\Z4we are going to install the awesome window manager, if you are on a vannila arch install, xorg will be installed as well!" 8 60
 }
+
+windowmanagerconfig || error "User exitted"
 
 installawesomexorg() { \
 	echo "##################################"
 	echo "##########installaing awesome#####"
 	sudo pacman -S xorg-server xorg-xinit xterm awesome sddm
 	sudo systemctl enable sddm
-addrepo || error "Error adding DTOS repo to /etc/pacman.conf."
+}
+
+installawesomexorg || error "User Ctrl+C"
